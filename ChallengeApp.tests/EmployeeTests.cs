d77499c47cd373922bf67dc46c5934cdb2c4059e
Blazej1997 <blazej.widera97@gmail.com>
2024-01-29 20:13:34 +0100

    dzien 7

---
+++ ChallengeApp.tests/EmployeeTests.cs
@@ -0,0 +1,54 @@
+namespace ChallengeApp.tests
+{
+    public class Tests
+    {
+        
+        [Test]
+        public void WhenAddScore_SholudHaveResult()
+        {
+            //arrange
+            var employee = new Employee("Marcin", "Kowalski", 28);
+            employee.AddScore(5);
+            employee.AddScore(6);
+            employee.AddScore(0);
+            employee.AddScore(10);
+            // act
+            var result = employee.Result;
+            //assert
+            Assert.AreEqual(21, result);
+
+        }
+        [Test]
+        public void WhenCollectScore_ScholdCorectResult()
+        {
+            var employee = new Employee("Magda", "Wróbel", 20);
+            employee.AddScore(10);
+            employee.AddScore(10);
+            employee.AddScore(9);
+            employee.AddScore(-20);
+
+            var result = employee.Result;
+
+            Assert.AreEqual(9, result);
+        }
+
+
+        [Test]
+        public void WhenAddedAndSubtrackPoints()
+        {
+            var employee = new Employee("Dawid", "Kowalski", 40);
+            employee.AddScore(20);
+            employee.AddScore(-20);
+            employee.AddScore(40);
+            employee.AddScore(100);
+
+            var result = employee.Result;
+
+            Assert.AreEqual(140, result);
+
+
+        }
+
+            
+    }
+}
